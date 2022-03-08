e6a7c82ba3ac13d5f6e119ba2b4f6258eeebb13b
Medokins <nikodemszwast@gmail.com>
2022-03-08 18:40:26 +0100

    use rio package

--- main.r
+++ main.r
@@ -1,9 +1,12 @@
-data = read.csv("C:\\Users\\medok\\OneDrive\\Desktop\\Python\\Leauge of Legends\\forCurious\\datasets\\LeaugeOfLegends.csv", header=T, sep=",")
+pacman::p_load(pacman, rio) 
+
+data <- import("C:\\Users\\medok\\OneDrive\\Desktop\\Python\\Leauge of Legends\\forCurious\\datasets\\LeaugeOfLegends.csv")
 
 head(data)
 hist(data$blueTotalJungleMinionsKilled)
  
 rm(list = ls()) 
+p_unload(all)
 
 detach("package:datasets", unload = TRUE)
 dev.off()
